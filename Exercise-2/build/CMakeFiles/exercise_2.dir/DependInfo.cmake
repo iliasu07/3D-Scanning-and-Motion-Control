a3909e828511019976d9606f233277abf76a57db
Iliasu Salaudeen <salaudeenilyas@gmail.com>
2025-11-07 23:42:49 +0100

    implicit surfaces

---
+++ Exercise-2/build/CMakeFiles/exercise_2.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/iliasu07/Documents/GitHub/TUM/3D-Scanning-and-Motion-Control/Exercise-2/Volume.cpp" "CMakeFiles/exercise_2.dir/Volume.cpp.o" "gcc" "CMakeFiles/exercise_2.dir/Volume.cpp.o.d"
+  "/home/iliasu07/Documents/GitHub/TUM/3D-Scanning-and-Motion-Control/Exercise-2/main.cpp" "CMakeFiles/exercise_2.dir/main.cpp.o" "gcc" "CMakeFiles/exercise_2.dir/main.cpp.o.d"
+  "" "exercise_2" "gcc" "CMakeFiles/exercise_2.dir/link.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ Exercise-2/build/CMakeFiles/exercise_2.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/exercise_2.dir/link.d"
+  "CMakeFiles/exercise_2.dir/Volume.cpp.o"
+  "CMakeFiles/exercise_2.dir/Volume.cpp.o.d"
+  "CMakeFiles/exercise_2.dir/main.cpp.o"
+  "CMakeFiles/exercise_2.dir/main.cpp.o.d"
+  "exercise_2"
+  "exercise_2.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/exercise_2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
